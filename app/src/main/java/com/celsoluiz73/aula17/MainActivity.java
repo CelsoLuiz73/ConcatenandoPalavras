822b93ed8c621771b4f44c298ee1513b6656c156
CelsoLuiz73 <oclinfo21@gmail.com>
2018-11-06 22:34:04 -0200

    final

--- app/src/main/java/com/celsoluiz73/aula17/MainActivity.java
+++ app/src/main/java/com/celsoluiz73/aula17/MainActivity.java
@@ -1,13 +1,69 @@
 package com.celsoluiz73.aula17;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout palavra01;
+    private TextInputLayout palavra02;
+    private Button btn_concatenar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        palavra01 = (TextInputLayout) findViewById(R.id.palavra01);
+        palavra02 = (TextInputLayout) findViewById(R.id.palavra02);
+        btn_concatenar = (Button) findViewById(R.id.btn_Concatenar);
+
+        btn_concatenar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        palavra01.getEditText().getText().toString() + palavra02.getEditText().getText().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
